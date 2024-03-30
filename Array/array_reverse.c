ff4c8386057ae4e74e6591ffd3a58836a281f0de
Angelica Ferriol <142831042+angelicaferriol@users.noreply.github.com>
2024-03-30 21:08:24 +0800

    Initial Commit

--- Array/array_reverse.c
+++ Array/array_reverse.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-void REVERSE(int X[], int Y[], int N) {
-    for (int i = 0; i < N; i++) {
+void REVERSE(int X[], int Y[], int N) 
+{
+    for (int i = 0; i < N; i++) 
+    {
         Y[i] = X[N - 1 - i];
     }
 }
 
-int main() {
+int main() 
+{
     // Example usage
     int X[] = {1, 2, 3, 4, 5};
     int N = sizeof(X) / sizeof(X[0]);
