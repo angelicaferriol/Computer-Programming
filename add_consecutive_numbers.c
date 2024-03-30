69c9aed00bddae5eaec05e82f3490fd10525c38e
Angelica Ferriol <142831042+angelicaferriol@users.noreply.github.com>
2024-03-30 20:31:28 +0800

    add_consecutive_numbers.c

---
+++ add_consecutive_numbers.c
@@ -0,0 +1,38 @@
+// Instruction: Input 10 numbers in an array called setNumber1. In another array called setNumber2 where the elements can be derived from the addition of two consecutive elements of setNumber1. Print the arrays.
+
+#include <stdio.h>
+
+int main()
+{
+	//initializing the two arrays
+	int setNumber1[10];
+	int setNumber2[10];
+	
+	//prompting the user input
+	printf("Input 10 integers: ");
+	
+	//using for loop to scan the array elements
+	for (int i=0; i<10; i++)
+	{
+		scanf("%d", &setNumber1[i]);
+	}	
+	
+	//printing array setNumber1
+	printf("setNumber1: ");
+	for (int i=0; i<10; i++)
+	{
+		printf("%3d", setNumber1[i]);
+	}
+	
+	//printing a line break between the two arrays
+	printf("\n");
+	
+	//printing array setNumber2	
+	printf("setNumber2: ");
+	for (int i=0; i<9; i++)
+	{
+		printf("%3d", setNumber1[i]+setNumber1[i+1]); 
+	}
+	
+	return 0;
+}
