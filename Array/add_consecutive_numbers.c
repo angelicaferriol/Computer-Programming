363567c1149591062412ec16dcd2252e40913a3d
Angelica Ferriol <142831042+angelicaferriol@users.noreply.github.com>
2024-03-30 21:09:15 +0800

    Initial Commit

--- Array/add_consecutive_numbers.c
+++ Array/add_consecutive_numbers.c
@@ -2,30 +2,30 @@
 
 int main()
 {
-	//initializing the two arrays
+	// initializing the two arrays
 	int setNumber1[10];
 	int setNumber2[10];
 	
-	//prompting the user input
+	// prompting the user input
 	printf("Input 10 integers: ");
 	
-	//using for loop to scan the array elements
+	// using for loop to scan the array elements
 	for (int i=0; i<10; i++)
 	{
 		scanf("%d", &setNumber1[i]);
 	}	
 	
-	//printing array setNumber1
+	// printing array setNumber1
 	printf("setNumber1: ");
 	for (int i=0; i<10; i++)
 	{
 		printf("%3d", setNumber1[i]);
 	}
 	
-	//printing a line break between the two arrays
+	// printing a line break between the two arrays
 	printf("\n");
 	
-	//printing array setNumber2	
+	// printing array setNumber2	
 	printf("setNumber2: ");
 	for (int i=0; i<9; i++)
 	{
